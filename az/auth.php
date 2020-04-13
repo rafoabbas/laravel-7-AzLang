25b9506b5b2262ad63889e396d83fd51edc7fde7
Rauf Abbas <rafo.abbas@gmail.com>
2020-04-13 22:25:59 +0300

    auth, pagination, passwords tərcümə edildi

---
+++ az/auth.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Kimlik kontrol metinleri
+    |--------------------------------------------------------------------------
+    |
+    | Aşağıdaki metinler kimlik doğrulama (giriş) sırasında kullanıcılara
+    | gösterilebilecek mesajlardır. Bu metinleri uygulamanızın
+    | gereksinimlerine göre düzenlemekte özgürsünüz.
+    |
+    */
+
+    'failed' => 'Girilmiş olduğun istifadəçi veriləri sistemdəkilər ile uyğun gəlmir.',
+    'throttle' => 'Sisteme çox giriş etmek istediniz. Xahiş olunur :seconds saniyə təkrar yoxlayın.',
+];
---
+++ az/passwords.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Parola hatırlatma metinleri
+    |--------------------------------------------------------------------------
+    |
+    | Aşağıdaki metinler, başarısız olmuş bir parola sıfırlama
+    | işleminde oluşan hataları temsil eden mesajlardır.
+    |
+    */
+
+    'reset' => 'Şifrəniz sıfırlandı!',
+    'sent' => 'Şifrə sıfırlama bağlantınız e-posta ilə göndərildi!',
+    'token' => 'Şifrə sıfırlama adresi/kodu etibarsızdır.',
+    'user' => 'Bu e-mail ünvanı ilə qeydiyyatdan keçmiş istifadəçi yoxdur.',
+    'throttled' => 'Xahiş edilir bir daha cəhd etmədən əvvəl gözləyinn.',
+];
